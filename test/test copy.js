d17c4b0d19a5f963c51211c78f884ce3eff51583
Olle Bergkvist <ollebergkvist@gmail.com>
2020-10-31 10:17:13 -0600

    Final test suite push

---
+++ test/test copy.js
@@ -0,0 +1,122 @@
+/**
+ * Selenium test suite
+ */
+"use strict";
+
+const assert = require("assert");
+const test = require("selenium-webdriver/testing");
+const firefox = require("selenium-webdriver/firefox");
+const webdriver = require("selenium-webdriver");
+const By = webdriver.By;
+
+let browser;
+
+// Helper functions
+async function goToNavLink(target) {
+  await browser.findElement(By.linkText(target)).then(function(element) {
+    element.click();
+  });
+}
+
+async function matchUrl(target) {
+  await browser.getCurrentUrl().then(function(url) {
+    assert.ok(url.endsWith(target));
+  });
+}
+
+async function assertH4(target) {
+  await browser.findElement(By.css("h4")).then(function(element) {
+    element.getText().then(function(text) {
+      assert.equal(text, target);
+    });
+  });
+}
+
+async function navigationLinks() {
+  let login = await browser.findElement(By.linkText("Login"));
+  let register = await browser.findElement(By.linkText("Register"));
+
+  assert.ok(login);
+  assert.ok(register);
+}
+
+// Test suite
+test.describe("Test suite me-vue-app", function() {
+  this.timeout(0);
+
+  test.beforeEach(async function(done) {
+    browser = new webdriver.Builder()
+      .withCapabilities(webdriver.Capabilities.firefox())
+      .setFirefoxOptions(new firefox.Options().headless())
+      .forBrowser("firefox")
+      .build();
+
+    await browser.get("https://app-trading.ollebergkvist.me");
+    done();
+  });
+
+  afterEach(function(done) {
+    browser.quit();
+    done();
+  });
+
+  // Test functions
+  async function register() {
+    let email = "test12345@gmail.com";
+    let password = "testtest";
+    let firstname = "test";
+    let lastname = "test";
+
+    const element = await browser.findElement(By.id("input-1"));
+    await element.sendKeys(email);
+
+    const element2 = await browser.findElement(By.id("input-2"));
+    await element2.sendKeys(password, Key.RETURN);
+
+    const element3 = await browser.findElement(By.id("input-3"));
+    await element3.sendKeys(firstname);
+
+    const element4 = await browser.findElement(By.id("input-4"));
+    await element4.sendKeys(lastname);
+
+    const element5 = browser.findElement(By.id("button"));
+    await element5.click();
+
+    browser.wait(until.elementLocated(By.id("h4-login")));
+  }
+
+  // Test case #1
+  test.it("Test index route", function(done) {
+    matchUrl("/");
+    assertH4("Log in");
+    done();
+  });
+
+  // Test case #2
+  test.it("Test register route", function(done) {
+    goToNavLink("Register");
+    matchUrl("/register");
+    assertH4("Register account");
+    done();
+  });
+
+  // Test case #3
+  test.it("Test to register account", function(done) {
+    goToNavLink("Register");
+    register();
+    done();
+  });
+
+  // // Test case #4
+  test.it("Test navbar", function(done) {
+    navigationLinks();
+    done();
+  });
+
+  // Test case #4
+  test.it("Test to find logtype", function(done) {
+    const logotype = browser.findElement(By.id("logotype"));
+    assert(logotype);
+    done();
+  });
+});
