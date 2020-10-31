4c89b263bdf24947334c4fbec439b433232f09a6
Olle Bergkvist <ollebergkvist@gmail.com>
2020-10-31 09:20:56 -0600

    test case 4 added

--- test/test.js
+++ test/test.js
@@ -67,29 +67,29 @@ test.describe("Test suite me-vue-app", function() {
   //   browser.wait(until.elementLocated(By.id("h4-account")));
   // }
 
-  // async function register() {
-  //   let email = "test12345@gmail.com";
-  //   let password = "testtest";
-  //   let firstname = "test";
-  //   let lastname = "test";
+  async function register() {
+    let email = "test12345@gmail.com";
+    let password = "testtest";
+    let firstname = "test";
+    let lastname = "test";
 
-  //   const element = await browser.findElement(By.id("input-1"));
-  //   await element.sendKeys(email);
+    const element = await browser.findElement(By.id("input-1"));
+    await element.sendKeys(email);
 
-  //   const element2 = await browser.findElement(By.id("input-2"));
-  //   await element2.sendKeys(password, Key.RETURN);
+    const element2 = await browser.findElement(By.id("input-2"));
+    await element2.sendKeys(password, Key.RETURN);
 
-  //   const element3 = await browser.findElement(By.id("input-3"));
-  //   await element3.sendKeys(firstname);
+    const element3 = await browser.findElement(By.id("input-3"));
+    await element3.sendKeys(firstname);
 
-  //   const element4 = await browser.findElement(By.id("input-4"));
-  //   await element4.sendKeys(lastname);
+    const element4 = await browser.findElement(By.id("input-4"));
+    await element4.sendKeys(lastname);
 
-  //   const element5 = browser.findElement(By.id("button"));
-  //   await element5.click();
+    const element5 = browser.findElement(By.id("button"));
+    await element5.click();
 
-  //   browser.wait(until.elementLocated(By.id("h4-login")));
-  // }
+    browser.wait(until.elementLocated(By.id("h4-login")));
+  }
 
   async function navigationLinks() {
     let login = await browser.findElement(By.linkText("Login"));
@@ -97,6 +97,12 @@ test.describe("Test suite me-vue-app", function() {
 
     assert.ok(login);
     assert.ok(register);
+  }
+
+  async function getAltTag() {
+    const element = browser.findElement(By.tagName("img")).getAttribute("alt");
+    const expected = "Olle Bergkvist logotype";
+    assert.equal(expected, element);
   }
 
   // Test case #1
@@ -120,12 +126,18 @@ test.describe("Test suite me-vue-app", function() {
     done();
   });
 
-  // // Test case #4
-  // test.it("Test to register account", function(done) {
-  //   goToNavLink("Register");
-  //   register();
-  //   done();
-  // });
+  // Test case #4
+  test.it("Test to register account", function(done) {
+    goToNavLink("Register");
+    register();
+    done();
+  });
+
+  // Test case #5
+  test.it("Test to get alt tag of logotype", function(done) {
+    getAltTag();
+    done();
+  });
 
   // Test case #5
   // test.it("Test sign in", function(done) {
